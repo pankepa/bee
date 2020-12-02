95ebc11e20cdc5728c82996d3a86fdf1e98b678b
zhenhua.pan <zhenhua.pan@fenrir-inc.com.cn>
2020-12-02 17:35:01 +0800

    origin version

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  version: "3.5.0",
-  note: '增加达达配送支持',
-  subDomain: "beeorder", // 根据教程 https://www.it120.cc/help/qr6l4m.html 查看并设置你自己的 subDomain
+  version: "1.0.0",
+  subDomain: "taixiangning",
 }
